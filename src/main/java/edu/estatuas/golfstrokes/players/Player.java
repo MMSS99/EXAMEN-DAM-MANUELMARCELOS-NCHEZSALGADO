133873913190f9870b518747a7bcb7fd107ca9c8
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-04-04 12:40:45 +0200

    feat(players): :sparkles: añadir toStrings y getters necesarios para completarlo

--- src/main/java/edu/estatuas/golfstrokes/players/Player.java
+++ src/main/java/edu/estatuas/golfstrokes/players/Player.java
@@ -17,6 +17,18 @@ public class Player {
         return iniciales;
     }
 
+    public byte getHandicap(){
+        return handicap;
+    }
+
+    public int getPuntuacionTotal(){
+        return puntuacionTotal;
+    }
+
+    public int getPuntosStableford(){
+        return puntosStableford;
+    }
+
     //setters
     public void setPuntosStrokeplay(int puntosStrokeplay) {
         this.puntuacionTotal = puntosStrokeplay;
@@ -24,5 +36,11 @@ public class Player {
 
     public void setPuntosStableford(int puntosStableford) {
         this.puntosStableford = puntosStableford;
+    }
+
+
+    @Override
+    public String toString(){
+        return iniciales + " ---> H'cap: " + getHandicap() + " \tStrokes: " + getPuntuacionTotal() + " \t Stableford: " + getPuntosStableford();
     }
 }
