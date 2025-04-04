0d897ea79065caad1aa328f449dfe11f904bd28e
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-04-04 12:05:21 +0200

    feat(golfplay): :feat: añadir constructor básico ComputeCard

--- src/main/java/edu/estatuas/golfstrokes/golfplay/ComputeCard.java
+++ src/main/java/edu/estatuas/golfstrokes/golfplay/ComputeCard.java
@@ -1,4 +1,14 @@
 package edu.estatuas.golfstrokes.golfplay;
 
+import edu.estatuas.golfstrokes.scorecard.ScoreCard;
+
 public class ComputeCard {
+
+    ScoreCard scorecard;
+    GolfPlay modalidad;
+
+    ComputeCard(ScoreCard scorecard, GolfPlay modalidad) {
+        this.scorecard = scorecard;
+        this.modalidad = modalidad;
+    }
 }
