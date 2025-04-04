ff49d4ab07dfe92988c132bba2430c77e84c6b2a
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-04-04 13:14:04 +0200

    fear(scorecard): :sparkles: añadida la clase Hole

---
+++ src/main/java/edu/estatuas/golfstrokes/scorecard/Hole.java
@@ -0,0 +1,20 @@
+package edu.estatuas.golfstrokes.scorecard;
+
+public class Hole {
+    byte posicion;
+    byte par;
+
+
+    public Hole(byte posicion, byte par) {
+        this.posicion = posicion;
+        this.par = par;
+    }
+
+    public byte getPosicion() {
+        return posicion;
+    }
+
+    public byte getPar() {
+        return par;
+    }
+}
